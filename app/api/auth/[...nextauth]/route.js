f649221a9fff947265fa9f1a9928c05c7d870048
Sunny <shikharbakhda@gmail.com>
2024-04-02 13:15:15 -0700

    remove mongo to debug

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -14,35 +14,36 @@ const handler = NextAuth({
   callbacks: {
     async session({ session }) {
       // store the user id from MongoDB to session
-      const sessionUser = await User.findOne({ email: session.user.email });
-      session.user.id = sessionUser._id.toString();
-      session.user.accountType = sessionUser.accountType;
-      session.user.specialties = sessionUser.specialties;
+      // const sessionUser = await User.findOne({ email: session.user.email });
+      // session.user.id = sessionUser._id.toString();
+      // session.user.accountType = sessionUser.accountType;
+      // session.user.specialties = sessionUser.specialties;
 
       return session;
     },
     async signIn({ account, profile, user, credentials }) {
-      try {
-        await connectToDB();
+      return true
+      // try {
+      //   await connectToDB();
 
-        // check if user already exists
-        const userExists = await User.findOne({ email: profile.email });
+      //   // check if user already exists
+      //   const userExists = await User.findOne({ email: profile.email });
 
-        // if not, create a new document and save user in MongoDB
-        if (!userExists) {
-          await User.create({
-            email: profile.email,
-            name: profile.name,
-            image: profile.picture,
-            accountType: "Unspecified",
-          });
-        }
+      //   // if not, create a new document and save user in MongoDB
+      //   if (!userExists) {
+      //     await User.create({
+      //       email: profile.email,
+      //       name: profile.name,
+      //       image: profile.picture,
+      //       accountType: "Unspecified",
+      //     });
+      //   }
 
-        return true
-      } catch (error) {
-        console.log("Error checking if user exists: ", error.message);
-        return false
-      }
+      //   return true
+      // } catch (error) {
+      //   console.log("Error checking if user exists: ", error.message);
+      //   return false
+      // }
     },
   }
 })

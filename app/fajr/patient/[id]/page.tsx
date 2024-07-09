b16943c3519ce1637956bc3ceb7df0938e7a9eb6
Kyle Essenmacher <kessenmacher7832@gmail.com>
2024-07-09 12:22:47 -0400

    added in submenu for patient-overview to be accessed inside notes, patient overview, image gallery, and lab visits. Added padding to the patient so that the menu doesn't go over it

--- app/fajr/patient/[id]/page.tsx
+++ app/fajr/patient/[id]/page.tsx
@@ -1,13 +1,17 @@
-import { PatientForm } from "@/components/form/Fajr/PatientForm"
+// app/fajr/patient/[id]/page.tsx
+// current patient form
+import { PatientForm } from "@/components/form/Fajr/PatientForm";
+import PatientSubmenu from "@/components/PatientSubmenu";
+import React from "react";
 
 export default function Home({params}: {params: {id: string}}) {
-  return (
-      <div className="w-full max-w-4xl mx-auto">
-        <h1 className="text-3xl font-bold mb-8 text-center">Patient Form</h1>
-        <div className="border border-gray-300 p-8 bg-white shadow rounded-lg">
-          <PatientForm id={params.id} />
+    return (
+        <div className="w-full max-w-4xl mx-auto pb-16"> {/* Added bottom padding to avoid nav bar overlap */}
+            <PatientSubmenu />
+            <h1 className="text-3xl font-bold mb-8 text-center">Patient Form</h1>
+            <div className="border border-gray-300 p-8 bg-white shadow rounded-lg">
+                <PatientForm id={params.id} />
+            </div>
         </div>
-      </div>
-  );
+    );
 }
-

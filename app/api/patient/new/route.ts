da5a4922ac17793b6fb460a1edd2fac3bec16db4
Kyle Essenmacher <kessenmacher7832@gmail.com>
2024-07-24 21:52:20 -0400

    fixed creating a new patient POST call

--- app/api/patient/new/route.ts
+++ app/api/patient/new/route.ts
@@ -1,3 +1,4 @@
+// app/api/patient/new/route.ts
 import Patient, { IPatient } from "@/models/patient";
 import dbConnect from "@/utils/database";  // Correct import statement
 
@@ -6,34 +7,12 @@ export const POST = async (request: Request) => {
   console.log(patientData);
 
   try {
-    await dbConnect();  // Correct function call
+    await dbConnect();
 
-    // The following code is commented out, but it could be uncommented and adjusted
-    // if you need to handle both updating existing patients and creating new ones.
-
-    // let existingPatient = await Patient.findById(patientData._id);
-    // if (existingPatient) {
-    //   // Update existing patient
-    //   patientData.age = parseInt(patientData.age as any);
-    //   patientData.surgeryDate = new Date(patientData.surgeryDate as any);
-    //   patientData.medx = patientData.medx ? patientData.medx.map((med: any) => {
-    //     return {
-    //       medName: med.medName,
-    //       medDosage: med.medDosage,
-    //       medFrequency: med.medFrequency,
-    //     };
-    //   }) : [];
-    //   existingPatient.set(patientData);
-
-    //   // Update other properties as needed
-    //   await existingPatient.save();
-    //   return new Response(JSON.stringify(existingPatient), { status: 200 });
-    // } else {
     // Create new patient
     const newPatient = new Patient(patientData);
     await newPatient.save();
     return new Response(JSON.stringify(newPatient), { status: 201 });
-    // }
   } catch (error) {
     return new Response(`Failed to create patient: ${error}`, { status: 500 });
   }
--- components/form/Fajr/PatientForm.tsx
+++ components/form/Fajr/PatientForm.tsx
@@ -1,3 +1,4 @@
+// components/form/Fajr/PatientForm.tsx
 "use client"
 
 import { zodResolver } from "@hookform/resolvers/zod"
@@ -7,9 +8,7 @@ import * as React from "react"
 import { useForm } from "react-hook-form"
 import { Button } from "@/components/ui/button"
 
-import {
-    Form,
-} from "@/components/ui/form"
+import { Form } from "@/components/ui/form"
 import { NumericalFormField } from "../NumericalFormField"
 import { TextAreaFormField } from "../TextAreaFormField"
 import { MedicationSelection } from "../MedicationSelection"
@@ -19,7 +18,6 @@ import { PMHxSelect } from "../PMHxSelection"
 import { PSHxSelect } from "../PSHxSelection"
 import { convertToWebP, calculateFileHash } from "../../../utils/encryptPhoto";
 
-// https://www.behindthename.com/random/random.php?gender=both&number=2&sets=1&surname=&usage_ara=1
 const patientFormSchema = z.object({
     patientId: z.string(),
     age: z.number(),
@@ -67,188 +65,61 @@ const defaultValues: Partial<PatientFormValues> = {
     allergies: "",
     notes: "",
 }
+
 export function PatientForm({id}: {id: string} = {id: ''}) {
 
-    // set state form values
     const [patientData, setPatientData] = React.useState<Partial<PatientFormValues>>(defaultValues);
 
-    // update default values if id is not empty
     React.useEffect(() => {
-    if (id !== '') {
-        // fetch the patient-info data from the API
-        fetch(`/api/patient/${id}`)
-        .then(response => response.json())
-        .then(data => {
-            // update the form with the data
-            data.surgeryDate = new Date(data.surgeryDate);
-            data.medx = data.medx.map((med: any) => {
-                return {
-                    medName: med.medName,
-                    medDosage: med.medDosage,
-                    medFrequency: med.medFrequency,
-                };
-            });
-            // age
-            data.age = parseInt(data.age);
-            setPatientData(data);
-            console.log(data)
-        })
-        .catch(error => {
-            // show an alert with the error message
-            alert('Error: ' + error.message);
-        });
-    }}, [id]);
-
+        if (id !== '') {
+            fetch(`/api/patient/${id}`)
+                .then(response => response.json())
+                .then(data => {
+                    data.surgeryDate = new Date(data.surgeryDate);
+                    data.medx = data.medx.map((med: any) => {
+                        return {
+                            medName: med.medName,
+                            medDosage: med.medDosage,
+                            medFrequency: med.medFrequency,
+                        };
+                    });
+                    data.age = parseInt(data.age);
+                    setPatientData(data);
+                    console.log(data);
+                })
+                .catch(error => {
+                    alert('Error: ' + error.message);
+                });
+        }
+    }, [id]);
 
     const form = useForm<PatientFormValues>({
         resolver: zodResolver(patientFormSchema),
         defaultValues,
     });
 
-    // reset the form values if the patientData changes
     React.useEffect(() => {
         form.reset(patientData);
-        // console.log(JSON.stringify(patientData, null, 2));
     }, [patientData, form]);
 
-    // const handleIconClick = async () => {
-    //     const files = form.getValues('files');
-    //     if (files && files.length > 0) {
-    //         try {
-    //             const uploadPromises = files.map((file: { hash: any; encryptionKey: any }) =>
-    //                 fetch(`/api/patient/photos/${file.hash}`, {
-    //                     method: 'GET',
-    //                 }).then(async (response) => {
-    //                     console.log(response)
-    //                     if (!response.ok) {
-    //                         throw new Error(`Failed to upload file with hash: ${file.hash}`);
-    //                     }
-    //                     return response.arrayBuffer().then(buffer => ({
-    //                         buffer,
-    //                         encryptionKey: file.encryptionKey,
-    //                     }));
-    //                 })
-    //             );
-
-    //             // const encryptedFiles = await Promise.all(uploadPromises);
-    //             // console.log(encryptedFiles);
-
-    //             // const decryptedFiles = encryptedFiles.map(async ({ buffer, encryptionKey }) => {
-    //             //     const decryptedBuffer = await decryptPhoto(buffer, encryptionKey);
-    //             //     return new Blob([decryptedBuffer], { type: 'image/webp' });
-    //             // });
-    //             // console.log(decryptedFiles);
-
-    //             // const results = await Promise.all(decryptedFiles);
-    //             const results = await Promise.all(uploadPromises);
-    //             setPhotos(results);
-    //             setShowModal(true);
-    //             // console.log('Files decrypted successfully:', results);
-    //             // Do something with the decrypted files, e.g., display them in the UI
-    //         } catch (error) {
-    //             console.error('Error uploading or decrypting files:', error);
-    //         }
-    //     }
-    // };
-
-    // const closeModal = () => {
-    //     setShowModal(false);
-    // };
-    
     const onSubmit = async (data: PatientFormValues) => {
-        // console.log(data)
-        // const images: File[] = Object.keys(data.images).map(key => data.images[key]);
-        // const encryptionKey = generateEncryptionKey();
-        // let encryptedImages: { hash: string }[] = [];
-        // try {
-        //     let convertedFiles = await Promise.all(images.map(file => convertToWebP(file)));
-    
-        //     const fileHashes = await Promise.all(convertedFiles.map(file => calculateFileHash(file)));
-    
-        //     const encryptedFiles = await Promise.all(convertedFiles.map(file => encryptPhoto(file, encryptionKey)));
+        console.log('Form submitted:', data);
 
-        //     const formData = new FormData();
-        //     convertedFiles.forEach((file, index) => {
-        //         formData.append('file', file, `${fileHashes[index]}.webp`);
-        //     })
-        //     // encryptedFiles.forEach((encryptedFile, index) => {
-        //     //     formData.append('file', new Blob([encryptedFile]), `${fileHashes[index]}.webp`);
-        //     // });
-                
-        //     // Send encrypted files and encryption key to backend
-        //     // formData.append('encryptionKey', encryptionKey);
-                
-        //     const response = await fetch('/api/patient/photos', {
-        //         method: 'POST',
-        //         body: formData,
-        //     });
-                    
-        //     // Prep for uploading to mongoDB
-        //     encryptedImages = encryptedFiles.map((file, index) => ({ hash: fileHashes[index], encryptionKey }));
-        //     // encryptedImages = fileHashes.map((fileHash, index) => ({ hash: fileHash }));
+        const response = await fetch('/api/patient/new', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
 
-        //     if (!response.ok) {
-        //         throw new Error(`HTTP error! status: ${response.status}`);
-        //     }
-
-        //     const result = await response.json();
-        //     console.log(result);
-        // } catch (error) {
-        //     console.error('Error uploading encrypted files:', error);
-        // }
-        // // update the Patient object using the API
-
-        // // send a POST request to the /patient-info/new endpoint with the data
-        // // import the IPatient interface from the models/patient-info.ts file
-
-        // // send the request
-        // if (id !== '') {
-        //     await fetch(`/api/patient/${id}`, {
-        //         method: 'PATCH',
-        //         body: JSON.stringify({ ...data, files: encryptedImages }),
-        //         headers: {
-        //         'Content-Type': 'application/json',
-        //         },
-        //     })
-        //     .then(response => {
-        //         if (response.ok) {
-        //         // redirect the user to the dashboard
-        //         window.location.href = '/patient-info/dashboard';
-        //         } else {
-        //         // show an alert with the error message
-        //         alert('Error: ' + response.statusText);
-        //         }
-        //     })
-        //     .catch(error => {
-        //         // show an alert with the error message
-        //         alert('Error: ' + error.message);
-        //     });
-        // } else {
-        //     await fetch('/api/patient/new', {
-        //         method: 'POST',
-        //         body: JSON.stringify({ ...data, files: encryptedImages }),
-        //         headers: {
-        //         'Content-Type': 'application/json',
-        //         },
-        //     })
-        //     .then(response => {
-        //         if (response.ok) {
-        //         // redirect the user to the dashboard
-        //         window.location.href = '/patient-info/dashboard';
-        //         } else {
-        //         // show an alert with the error message
-        //         alert('Error: ' + response.statusText);
-        //         }
-        //     })
-        //     .catch(error => {
-        //         // show an alert with the error message
-        //         alert('Error: ' + error.message);
-        //     });
-        // }
-
-
-        // show a popup with the values
-        // alert(JSON.stringify(data, null, 2));
+        if (response.ok) {
+            console.log('Patient created successfully');
+            window.location.href = '/patient-info/dashboard';
+        } else {
+            console.error('Error creating patient:', response.statusText);
+            alert('Error: ' + response.statusText);
+        }
     };
 
     return (
@@ -266,7 +137,7 @@ export function PatientForm({id}: {id: string} = {id: ''}) {
                     <TextFormField form={form} fieldName="occupation" fieldLabel="Job/Occupation" />
                     <TextAreaFormField form={form} fieldName="diagnosis" fieldLabel="Patient Diagnosis" />
                     <TextAreaFormField form={form} fieldName="icd10" fieldLabel="ICD-10" />
-                            <DatePickerFormField form={form} fieldName="surgeryDate" fieldLabel="Date of Surgery" />
+                    <DatePickerFormField form={form} fieldName="surgeryDate" fieldLabel="Date of Surgery" />
                     <div className="flex space-x-4">
                         <div className="w-1/2 space-y-3">
                             <SelectFormField form={form} fieldName="laterality" fieldLabel="Laterality" selectOptions={['Bilateral', 'Left', 'Right']} />
@@ -277,15 +148,6 @@ export function PatientForm({id}: {id: string} = {id: ''}) {
                             <SelectFormField form={form} fieldName="hospital" fieldLabel="Hospital" selectOptions={['PMC', 'PRCS', 'Hugo Chavez']} />
                         </div>
                     </div>
-                    <div className="flex space-x-4">
-                        <div className="w-1/2">
-                            
-                        </div>
-                        <div className="w-1/2">
-                        </div>
-                    </div>
-
-
                     <MedicationSelection form={form} fieldName="medx" fieldLabel="Medications Needed" />
                     <PMHxSelect form={form} fieldName="pmhx" fieldLabel="PMHx" fieldCompact="PMHx" PopOverComponent={null} />
                     <PSHxSelect form={form} fieldName="pshx" fieldLabel="PSHx" fieldCompact="PSHx" PopOverComponent={null} />
@@ -298,11 +160,8 @@ export function PatientForm({id}: {id: string} = {id: ''}) {
                         </div>
                     </div>
                     <TextFormField form={form} fieldName="otherDrugs" fieldLabel="Other illicit uses" />
-                    
                     <TextFormField form={form} fieldName="allergies" fieldLabel="Allergies" />
-                    
                     <TextAreaFormField form={form} fieldName="notes" fieldLabel="Notes" />
-
                     <Button type="submit">Submit Request</Button>
                 </form>
             </Form>

997bdd856d0a76909be41208a280e1b9d4b31c9c
Kyle Essenmacher <kessenmacher7832@gmail.com>
2024-08-13 17:12:19 -0400

    added in the ability to track the users location for entering in the location on the new patient form

--- components/form/TextFormField.tsx
+++ components/form/TextFormField.tsx
@@ -2,18 +2,18 @@
 import { FormField, FormItem, FormLabel, FormControl } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 
-export function TextFormField({form, fieldName, fieldLabel}: {form: any, fieldName: string, fieldLabel: string}) {
+export function TextFormField({form, fieldName, fieldLabel, className}: {form: any, fieldName: string, fieldLabel: string, className?: string}) {
     return (
         <FormField
             control={form.control}
             name={fieldName}
             render={({ field }) => (
-                <FormItem>
+                <FormItem className={className}>
                     <FormLabel>{fieldLabel}</FormLabel>
                     <FormControl>
-                        <Input {...field} />
+                        <Input {...field} className="w-full" />
                     </FormControl>
                 </FormItem>
-        )}/>
+            )}/>
     );
 };

842f5f75ea9717412c6ec70d724f32d0a6bfcc36
Kyle Essenmacher <kessenmacher7832@gmail.com>
2024-08-21 22:21:31 -0400

    revertted the app/create-patient/[id] route and made it app/create-patient again

--- app/create-patient/page.jsx
+++ app/create-patient/page.jsx
@@ -1,16 +1,15 @@
-// app/create-patient/[id]/page.jsx
+// app/create-patient/page.jsx
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { BarLoader } from "react-spinners";
 import NewPatientForm from "@/components/form/NewPatientForm";
 import ConfirmationModal from "@/components/ConfirmationModal";
 import ErrorModal from "@/components/ErrorModal";
 
-const CreatePatient = ({ params }) => {
+const CreatePatient = () => {
   const router = useRouter();
-  const { id } = params;
 
   const [submitting, setIsSubmitting] = useState(false);
   const [showModal, setShowModal] = useState(false);
@@ -18,35 +17,13 @@ const CreatePatient = ({ params }) => {
   const [patientData, setPatientData] = useState(null);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchPatientData = async () => {
-      try {
-        const response = await fetch(`/api/patient/${id}`);
-        if (response.ok) {
-          const data = await response.json();
-          setPatientData(data);
-        } else {
-          throw new Error("Failed to fetch patient data");
-        }
-      } catch (error) {
-        console.error("Error fetching patient data:", error);
-        setError("Error fetching patient data");
-        setShowErrorModal(true);
-      }
-    };
-
-    if (id) {
-      fetchPatientData();
-    }
-  }, [id]);
-
-  const updatePatient = async (formData) => {
+  const createPatient = async (formData) => {
     setIsSubmitting(true);
     setError(null);
 
     try {
-      const response = await fetch(`/api/patient/${id}`, {
-        method: "PATCH",
+      const response = await fetch(`/api/patient/new`, {
+        method: "POST",
         body: JSON.stringify(formData),
         headers: {
           "Content-Type": "application/json",
@@ -57,14 +34,14 @@ const CreatePatient = ({ params }) => {
         const data = await response.json();
         setPatientData({ firstName: formData.firstName, lastName: formData.lastName });
         setShowModal(true);
+        router.push(`/create-patient/${data._id}`);  // Redirect with the new patient ID
       } else {
         const errorMessage = await response.text();
-        setError(`Failed to update patient: ${errorMessage}`);
-        console.error("Failed to update patient:", errorMessage);
+        setError(`Failed to create patient: ${errorMessage}`);
         setShowErrorModal(true);
       }
     } catch (error) {
-      console.log(`Error: ${error}`);
+      console.error("Error creating patient:", error);
       setError(`An error occurred: ${error.message}`);
       setShowErrorModal(true);
     } finally {
@@ -81,10 +58,6 @@ const CreatePatient = ({ params }) => {
     setShowErrorModal(false);
   };
 
-  const goToHome = () => {
-    router.push("/");
-  };
-
   return (
       <div className="w-full max-w-4xl mx-auto">
         <h1 className="text-3xl font-bold mb-8 text-center">New Patient Form</h1>
@@ -93,25 +66,17 @@ const CreatePatient = ({ params }) => {
               <div className="flex justify-center">
                 <BarLoader color="#FF5722" />
               </div>
-          ) : patientData ? (
-              <NewPatientForm
-                  handleSubmit={updatePatient}
-                  submitting={submitting}
-                  email={patientData.email}
-                  password={patientData.password}
-                  firstName={patientData.firstName}
-                  lastName={patientData.lastName}
-              />
           ) : (
-              <p>Loading patient data...</p>
+              <NewPatientForm
+                  handleSubmit={createPatient}
+                  submitting={submitting}
+              />
           )}
         </div>
         {showModal && (
             <ConfirmationModal
-                patientId={id}
                 patientName={{ firstName: patientData?.firstName, lastName: patientData?.lastName }}
                 onClose={handleModalClose}
-                onHome={goToHome}
             />
         )}
         {showErrorModal && (
--- models/patient.ts
+++ models/patient.ts
@@ -1,93 +1,84 @@
-  // models/patient.ts
-  import { Schema, model, models, Document, Types } from 'mongoose';
-  import { SPECIALTIES } from '@/data/data';
+// models/patient.ts
+import { Schema, model, models, Document, Types } from 'mongoose';
+import { SPECIALTIES } from '@/data/data';
 
-  interface Prescription {
-    medName: string;
-    medDosage: string;
-    medFrequency: string;
-  }
+interface Prescription {
+  medName: string;
+  medDosage: string;
+  medFrequency: string;
+}
 
-  export interface IPatient extends Document {
-    files: any[];
-    patientId?: string;
-    _id?: string;
-    firstName: string;
-    lastName: string;
-    phone?: string;
-    age?: string;
-    location?: string;
-    language?: string;
-    chiefComplaint?: string;
-    coordinatorId?: string;
-    password: string;
-    email: string;
-    laterality?: 'Not Selected' | 'Left' | 'Right' | 'Bilateral';
-    diagnosis?: string;
-    diagnosisCat?: string;
-    hospital?: 'Not Selected' | 'PMC' | 'PRCS' | 'Hugo Chavez';
-    priority?: 'Not Selected' | 'Routine' | 'Moderate' | 'Urgent' | 'Emergency';
-    specialty?: typeof SPECIALTIES[number];
-    status?: 'Not Selected' | 'Not Started' | 'Triaged' | 'In-Progress' | 'Completed';
-    name?: string; // can get rid of since we have firstname and Lastname
-    complaint?: string;
-    icd10?: string;
-    surgeryDate?: Date;
-    occupation?: string;
-    baselineAmbu?: 'Not Selected' | 'Independent' | 'Boot' | 'Crutches' | 'Walker' | 'Non-Ambulatory';
-    pmhx?: string[];
-    pshx?: string[];
-    medx?: Prescription[];
-    smokeCount?: string;
-    drinkCount?: string;
-    otherDrugs?: string;
-    allergies?: string;
-    triagedBy?: string;
-    doctor?: string;
-    notes?: string;
-    visits?: Types.ObjectId[];
-  }
+export interface IPatient extends Document {
+  files: any[];
+  _id?: string;
+  firstName: string;
+  lastName: string;
+  phone?: string;
+  age?: string;
+  location?: string;
+  language?: string;
+  chiefComplaint?: string;
+  coordinatorId?: string;
+  laterality?: 'Not Selected' | 'Left' | 'Right' | 'Bilateral';
+  diagnosis?: string;
+  diagnosisCat?: string;
+  hospital?: 'Not Selected' | 'PMC' | 'PRCS' | 'Hugo Chavez';
+  priority?: 'Not Selected' | 'Routine' | 'Moderate' | 'Urgent' | 'Emergency';
+  specialty?: typeof SPECIALTIES[number];
+  status?: 'Not Selected' | 'Not Started' | 'Triaged' | 'In-Progress' | 'Completed';
+  name?: string;
+  complaint?: string;
+  icd10?: string;
+  surgeryDate?: Date;
+  occupation?: string;
+  baselineAmbu?: 'Not Selected' | 'Independent' | 'Boot' | 'Crutches' | 'Walker' | 'Non-Ambulatory';
+  pmhx?: string[];
+  pshx?: string[];
+  medx?: Prescription[];
+  smokeCount?: string;
+  drinkCount?: string;
+  otherDrugs?: string;
+  allergies?: string;
+  triagedBy?: string;
+  doctor?: string;
+  notes?: string;
+  visits?: Types.ObjectId[];
+}
 
-  // models/patient.ts
+const PatientSchema = new Schema<IPatient>({
+  files: [{ type: Object }],
+  firstName: { type: String, required: true },
+  lastName: { type: String },
+  phone: { type: String },
+  age: { type: String },
+  location: { type: String },
+  language: { type: String },
+  chiefComplaint: { type: String },
+  laterality: { type: String, enum: ['Not Selected', 'Left', 'Right', 'Bilateral'], default: 'Not Selected' },
+  diagnosis: { type: String },
+  diagnosisCat: { type: String },
+  hospital: { type: String, enum: ['Not Selected', 'PMC', 'PRCS', 'Hugo Chavez'], default: 'Not Selected' },
+  priority: { type: String, enum: ['Not Selected', 'Routine', 'Moderate', 'Urgent', 'Emergency'], default: 'Not Started' },
+  specialty: { type: String, enum: ['Not Selected', 'Podiatry', 'Sports', 'Vascular', 'General', 'Urology', 'OBGYN', 'Opthalmology', 'ENT', 'Joints', 'Pediatrics', 'Spine/Neurosurgery', 'Internal Medicine'], default: 'Not Started' },
+  icd10: { type: String },
+  surgeryDate: { type: Date },
+  occupation: { type: String },
+  baselineAmbu: { type: String, enum: ['Not Selected', 'Independent', 'Boot', 'Crutches', 'Walker', 'Non-Ambulatory'] },
+  pmhx: { type: [String], default: [] },
+  pshx: { type: [String], default: [] },
+  medx: [{
+    medName: { type: String },
+    medDosage: { type: String },
+    medFrequency: { type: String }
+  }],
+  smokeCount: { type: String },
+  drinkCount: { type: String },
+  otherDrugs: { type: String },
+  allergies: { type: String },
+  notes: { type: String },
+  visits: [{ type: Schema.Types.ObjectId, ref: 'Visit' }],
+});
 
-  const PatientSchema = new Schema<IPatient>({
-    files: [{ type: Object }],
-    firstName: { type: String, required: true },
-    lastName: { type: String },
-    phone: { type: String },
-    age: { type: String },
-    location: { type: String },
-    language: { type: String },
-    chiefComplaint: { type: String },
-    password: { type: String },
-    email: { type: String },
-    laterality: { type: String, enum: ['Not Selected', 'Left', 'Right', 'Bilateral'], default: 'Not Selected' },
-    diagnosis: { type: String },
-    diagnosisCat: { type: String },
-    hospital: { type: String, enum: ['Not Selected', 'PMC', 'PRCS', 'Hugo Chavez'], default: 'Not Selected' },
-    priority: { type: String, enum: ['Not Selected', 'Routine', 'Moderate', 'Urgent', 'Emergency'], default: 'Not Selected' },
-    specialty: { type: String, enum: ['Not Selected', 'Podiatry', 'Sports', 'Vascular', 'General', 'Urology', 'OBGYN', 'Opthalmology', 'ENT', 'Joints', 'Pediatrics', 'Spine/Neurosurgery', 'Internal Medicine'], default: 'Not Selected' },
-    status: { type: String, enum: ['Not Selected', 'Not Started', 'Triaged', 'In-Progress', 'Completed'], default: 'Not Started' },
-    icd10: { type: String },
-    surgeryDate: { type: Date },
-    occupation: { type: String },
-    baselineAmbu: { type: String, enum: ['Not Selected', 'Independent', 'Boot', 'Crutches', 'Walker', 'Non-Ambulatory'] },
-    pmhx: { type: [String], default: [] },
-    pshx: { type: [String], default: [] },
-    medx: [{
-      medName: { type: String },
-      medDosage: { type: String },
-      medFrequency: { type: String }
-    }],
-    smokeCount: { type: String },
-    drinkCount: { type: String },
-    otherDrugs: { type: String },
-    allergies: { type: String },
-    notes: { type: String },
-    visits: [{ type: Schema.Types.ObjectId, ref: 'Visit' }],
-    patientId: { type: String, sparse: true, unique: true }  // Mark as sparse to allow null values
-  });
+const Patient = models.Patient || model<IPatient>('Patient', PatientSchema);
 
-  const Patient = models.Patient || model<IPatient>('Patient', PatientSchema);
-
-  export default Patient;
+export default Patient;

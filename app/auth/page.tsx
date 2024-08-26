f99ca7ec68a4afd761fb6af106065501c099b3af
Kyle Essenmacher <kessenmacher7832@gmail.com>
2024-08-26 13:49:28 -0400

    changed the name to last name on the sign up form for doctors, updated the toast logic, made text more visible by adding a white background to the text boxes, added in root colors to the global CSS

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -23,9 +23,12 @@ const AuthPage = () => {
             <div className="absolute top-0 left-0 w-full h-full bg-transparent z-0">
                 <section className="container absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                     <div className="content">
-                        <div className="heart-rate w-full h-full">
+                        <div className="ball ball1"></div>
+                        <div className="ball ball2"></div>
+                        <div className="ball ball3"></div>
+                        <div className="heart-rate">
                             {/* Your SVG Heartbeat Monitor */}
-                            <svg viewBox="0 9 498.778 54.805" xmlns="http://www.w3.org/2000/svg" className={`w-full h-full opacity-100 ${showLoginModal ? 'blur-sm' : ''}`}>
+                            <svg viewBox="0 9 498.778 54.805" className={`w-full h-full opacity-100 ${showLoginModal ? 'blur-sm' : ''}`}>
                                 <polyline fill="none" stroke="#FF5722" strokeWidth="2" strokeMiterlimit="10" points="0 45.486 64.133 45.486 74.259 33.324 84.385 45.486 96.2 45.486 104.637 55.622 119.825 9 133.327 63.729 140.079 45.486 162.018 45.486 172.146 40.419 183.958 45.486 249.778 45.486" />
                                 <polyline fill="none" stroke="#FF5722" strokeWidth="2" strokeMiterlimit="10" points="249 45.562 313.133 45.562 323.259 33.4 333.385 45.562 345.2 45.562 353.637 55.698 368.825 9.076 382.327 63.805 389.079 45.562 411.018 45.562 421.146 40.495 432.958 45.562 498.778 45.562" />
                             </svg>
@@ -37,7 +40,7 @@ const AuthPage = () => {
                 </section>
             </div>
 
-            <div className="w-full max-w-md p-6 rounded shadow-md relative z-10 bg-white bg-opacity-30 backdrop-filter backdrop-blur-lg">
+            <div className="login-card w-full max-w-md p-6 rounded shadow-md relative z-10 bg-white bg-opacity-30 backdrop-filter backdrop-blur-lg">
                 <DoctorSignupForm />
             </div>
 

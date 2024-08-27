085bf07f79134f154453443984578648cb44d064
Kyle Essenmacher <kessenmacher7832@gmail.com>
2024-08-27 14:34:33 -0400

    fixed password hashing and got the login route working

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -61,7 +61,7 @@ export function LoginForm() {
         setSubmitting(true);
 
         try {
-            const response = await fetch('/api/auth/login/doctor', {
+            const response = await fetch('/api/auth/login/', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',

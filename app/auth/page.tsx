5265927d04d5f8ad57c927b5be1d58f721816c3d
Kyle Essenmacher <kessenmacher7832@gmail.com>
2024-09-04 09:40:15 -0400

    removed the animation on the auth screen. took out API calls not being used anymore

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -29,26 +29,30 @@ const AuthPage = () => {
     const ballsOpacity = authType === 'Login' ? 'opacity-50' : accountType === 'Doctor' ? 'opacity-20' : 'opacity-80';
 
     return (
-        <div className="h-screen p-4 bg-white flex flex-col items-center justify-center relative">
-            <div className="absolute top-0 left-0 w-full h-full bg-transparent z-0">
-                <section className="container absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-                    <div className="content">
-                        <div className={`ball ball1 ${ballsOpacity}`}></div>
-                        <div className={`ball ball2 ${ballsOpacity}`}></div>
-                        <div className={`ball ball3 ${ballsOpacity}`}></div>
-                        <div className={`ball ball4 ${ballsOpacity}`}></div>
-                        <div className={`ball ball5 ${ballsOpacity}`}></div>
-                        <div className="heart-rate">
-                            <svg viewBox="0 9 498.778 54.805" className={`w-full h-full ${heartRateOpacity}`}>
-                                <polyline fill="none" stroke="#FF5722" strokeWidth="2" strokeMiterlimit="10" points="0 45.486 64.133 45.486 74.259 33.324 84.385 45.486 96.2 45.486 104.637 55.622 119.825 9 133.327 63.729 140.079 45.486 162.018 45.486 172.146 40.419 183.958 45.486 249.778 45.486" />
-                                <polyline fill="none" stroke="#FF5722" strokeWidth="2" strokeMiterlimit="10" points="249 45.562 313.133 45.562 323.259 33.4 333.385 45.562 345.2 45.562 353.637 55.698 368.825 9.076 382.327 63.805 389.079 45.562 411.018 45.562 421.146 40.495 432.958 45.562 498.778 45.562" />
-                            </svg>
-                            <div className="fade-in"></div>
-                            <div className="fade-out"></div>
-                        </div>
-                    </div>
-                </section>
-            </div>
+        <div className="h-screen w-full p-4 bg-white flex flex-col items-center justify-center relative">
+        {/*    animation that i spent way too much time on that i don't want to see gone yet. this css in the global.css file broke it. .app:not(.dashboard-page) {*/}
+        {/*    padding: 1rem;*/}
+        {/*    max-width: max-content;*/}
+        {/*}*/}
+            {/*<div className="absolute top-0 left-0 w-full h-full bg-transparent z-0">*/}
+            {/*    <section className="container absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-3/4">*/}
+            {/*        <div className="content">*/}
+            {/*            <div className={`ball ball1 ${ballsOpacity}`}></div>*/}
+            {/*            <div className={`ball ball2 ${ballsOpacity}`}></div>*/}
+            {/*            <div className={`ball ball3 ${ballsOpacity}`}></div>*/}
+            {/*            <div className={`ball ball4 ${ballsOpacity}`}></div>*/}
+            {/*            <div className={`ball ball5 ${ballsOpacity}`}></div>*/}
+            {/*            <div className="heart-rate">*/}
+            {/*                <svg viewBox="0 9 498.778 54.805" className={`w-full h-full ${heartRateOpacity}`}>*/}
+            {/*                    <polyline fill="none" stroke="#FF5722" strokeWidth="2" strokeMiterlimit="10" points="0 45.486 64.133 45.486 74.259 33.324 84.385 45.486 96.2 45.486 104.637 55.622 119.825 9 133.327 63.729 140.079 45.486 162.018 45.486 172.146 40.419 183.958 45.486 249.778 45.486" />*/}
+            {/*                    <polyline fill="none" stroke="#FF5722" strokeWidth="2" strokeMiterlimit="10" points="249 45.562 313.133 45.562 323.259 33.4 333.385 45.562 345.2 45.562 353.637 55.698 368.825 9.076 382.327 63.805 389.079 45.562 411.018 45.562 421.146 40.495 432.958 45.562 498.778 45.562" />*/}
+            {/*                </svg>*/}
+            {/*                <div className="fade-in"></div>*/}
+            {/*                <div className="fade-out"></div>*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*    </section>*/}
+            {/*</div>*/}
 
             <div className="flex flex-col items-center justify-center w-full max-w-md">
                 <RadioCard.Root

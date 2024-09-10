2b84bcebd822eba0e5315cd42f4e699433b8e130
Kyle Essenmacher <kessenmacher7832@gmail.com>
2024-09-10 16:06:35 -0400

    got email sending to work upon account creation

--- components/auth/SignupForm.tsx
+++ components/auth/SignupForm.tsx
@@ -10,7 +10,6 @@ import { Form } from "@/components/ui/form";
 import { useRouter } from "next/navigation";
 import useToast from "@/components/hooks/useToast";
 import SecurityQuestionsForm from './SecurityQuestionsForm';
-import { sendWelcomeEmail } from '@/utils/email';
 
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 const passwordRegex = /^(?=.*[0-9]).{8,}$/;

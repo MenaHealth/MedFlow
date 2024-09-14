23321d1668d44e03925a05d1edb123965a6a80da
Kyle Essenmacher <kessenmacher7832@gmail.com>
2024-09-14 13:52:17 -0400

    have the forms all setup. need to get the API calls working now

--- components/ui/PasswordField.tsx
+++ components/ui/PasswordField.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import * as Form from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { EyeClosedIcon, EyeOpenIcon } from "@radix-ui/react-icons";
@@ -27,6 +27,7 @@ const PasswordField = ({
     const [isPasswordVisible, setIsPasswordVisible] = useState(false);
     const [isFocused, setIsFocused] = useState(false);
     const [showTooltip, setShowTooltip] = useState(false);
+    const [isValid, setIsValid] = useState(false);
 
     const id = `${fieldName}-input`;
 
@@ -46,45 +47,60 @@ const PasswordField = ({
         <Form.FormField
             control={form.control}
             name={fieldName}
-            render={({ field }) => (
-                <Form.FormItem className={`mb-6 p-2 ${className}`}>
-                    <div className="relative">
-                        <Input
-                            {...field}
-                            type={isPasswordVisible ? 'text' : 'password'}
-                            onFocus={handleFocus}
-                            onBlur={handleBlur}
-                            id={id}
-                            className={`w-full pt-4 pb-2 pl-2 pr-10 ${
-                                isFocused || field.value ? 'bg-white' : ''
-                            }`}
-                        />
-                        <Form.FormLabel htmlFor={id} className={`absolute transition-all ${
-                            (isFocused || field.value) ? 'text-xs -top-6' : 'text-sm top-1/2 -translate-y-1/2'
-                        } left-2 pointer-events-none`}>
-                            {fieldLabel}
-                        </Form.FormLabel>
-                        <button
-                            type="button"
-                            onMouseEnter={() => setIsPasswordVisible(true)}
-                            onMouseLeave={() => setIsPasswordVisible(false)}
-                            className="absolute right-2 top-1/2 transform -translate-y-1/2"
-                            tabIndex={-1}
-                        >
-                            {isPasswordVisible ? (
-                                <EyeOpenIcon className="h-5 w-5 text-gray-500" />
-                            ) : (
-                                <EyeClosedIcon className="h-5 w-5 text-gray-500" />
+            render={({ field }) => {
+                const handleChange = (event: any) => {
+                    field.onChange(event); // Call the default onChange function
+                    const regex = /^(?=.*[0-9]).{8,}$/;
+                    setIsValid(regex.test(event.target.value));
+                };
+
+                // Or, if you prefer to use useEffect:
+                useEffect(() => {
+                    const regex = /^(?=.*[0-9]).{8,}$/;
+                    setIsValid(regex.test(field.value));
+                }, [field.value]);
+
+                return (
+                    <Form.FormItem className={`mb-6 p-2 ${className} ${isValid ? 'bg-green-100' : ''}`}>
+                        <div className="relative">
+                            <Input
+                                {...field}
+                                type={isPasswordVisible ? 'text' : 'password'}
+                                onFocus={handleFocus}
+                                onBlur={handleBlur}
+                                onChange={handleChange}
+                                id={id}
+                                className={`w-full pt-4 pb-2 pl-2 pr-10 ${
+                                    isFocused || field.value ? 'bg-white' : ''
+                                }`}
+                            />
+                            <Form.FormLabel htmlFor={id} className={`absolute transition-all ${
+                                (isFocused || field.value) ? 'text-xs -top-6' : 'text-sm top-1/2 -translate-y-1/2'
+                            } left-2 pointer-events-none`}>
+                                {fieldLabel}
+                            </Form.FormLabel>
+                            <button
+                                type="button"
+                                onMouseEnter={() => setIsPasswordVisible(true)}
+                                onMouseLeave={() => setIsPasswordVisible(false)}
+                                className="absolute right-2 top-1/2 transform -translate-y-1/2"
+                                tabIndex={-1}
+                            >
+                                {isPasswordVisible ? (
+                                    <EyeOpenIcon className="h-5 w-5 text-gray-500" />
+                                ) : (
+                                    <EyeClosedIcon className="h-5 w-5 text-gray-500" />
+                                )}
+                            </button>
+                            {showTooltip && (
+                                <div className="absolute left-0 top-full w-full bg-gray-700 text-white text-sm p-2 rounded shadow-lg z-10">
+                                    Eight or more characters and at least one number
+                                </div>
                             )}
-                        </button>
-                        {showTooltip && (
-                            <div className="absolute left-0 bottom-full mt-2 w-full bg-gray-700 text-white text-sm p-2 rounded shadow-lg z-10">
-                                Eight or more characters and at least one number
-                            </div>
-                        )}
-                    </div>
-                </Form.FormItem>
-            )}
+                        </div>
+                    </Form.FormItem>
+                );
+            }}
         />
     );
 };

60446df3c93b53ee7c252d3dd84c03ad642a2b96
Kyle Essenmacher <kessenmacher7832@gmail.com>
2024-09-26 18:29:37 -0400

    update middleware to allow the signup success screen to show when not logged in

--- middleware.ts
+++ middleware.ts
@@ -8,19 +8,19 @@ export async function middleware(req: any) {
     const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
     const { pathname } = req.nextUrl;
 
-    // Allow requests to static files, API routes, and the auth page
+    // Allow requests to static files, API routes, and any route under '/auth'
     if (
         pathname.startsWith('/api') ||          // Allow API routes
-        pathname === '/auth' ||                 // Allow the auth page
+        pathname.startsWith('/auth') ||         // Allow any route under /auth
         pathname.match(PUBLIC_FILE) ||          // Allow static files (e.g., .css, .js, .png, .jpg, etc.)
         pathname === '/create-patient' ||       // Allow the new patient form
-        pathname === '/about'                // Allow the about page
+        pathname === '/about'                   // Allow the about page
     ) {
         return NextResponse.next();
     }
 
     // Redirect to auth page if user is not authenticated and trying to access protected routes
-    if (!(token || pathname === '/auth')) {
+    if (!token) {
         return NextResponse.redirect(new URL('/auth', req.url));
     }
 

4a375dd27b427815279e4503fad7f4f364f52d78
mhatt212 <162051691+mhatt212@users.noreply.github.com>
2024-10-06 13:19:58 -0400

    Update page.tsx

--- app/rx/[id]/page.tsx
+++ app/rx/[id]/page.tsx
@@ -4,6 +4,7 @@
 
 import { useSession } from "next-auth/react";
 import { useRouter } from 'next/navigation';
+import RXForm from "@/components/form/RXForm;
 
 interface RXPageProps {
     params: {

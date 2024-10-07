a94a97455ccb986374ce2fcb0ab5ad45cad3a820
mhatt212 <162051691+mhatt212@users.noreply.github.com>
2024-10-07 09:25:03 -0400

    Update page.tsx

--- app/rx/[id]/page.tsx
+++ app/rx/[id]/page.tsx
@@ -30,7 +30,6 @@ const RXPage: React.FC<RXPageProps> = ({ params }) => {
         <div className="w-full max-w-4xl mx-auto pb-16">
             <h1 className="text-3xl font-bold mb-8 text-center">RX Page</h1>
             <div className="border border-gray-300 p-8 bg-white shadow rounded-lg">
-                <p>Welcome, {username}</p>
                 {/* Render the RXForm here */}
                 <RXForm patientId={patientId} username={username} />
             </div>

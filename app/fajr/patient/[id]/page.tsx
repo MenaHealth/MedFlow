3831229d0d3ca37df924f14d80b65f2e00ea2988
mhatt212 <162051691+mhatt212@users.noreply.github.com>
2024-10-09 21:40:43 -0400

    Update page.tsx

--- app/fajr/patient/[id]/page.tsx
+++ app/fajr/patient/[id]/page.tsx
@@ -7,7 +7,7 @@ export default function Home({params}: {params: {id: string}}) {
     return (
         <div className="w-full max-w-4xl mx-auto pb-16">
             <PatientSubmenu />
-            <h1 className="text-3xl font-bold mb-8 text-center">Patient Information</h1>
+            <h1 className="text-3xl font-bold mb-8 m-8 text-center">Patient Information</h1>
             <div className="border border-gray-300 p-8 bg-white shadow rounded-lg">
                 <PatientForm id={params.id} />
             </div>

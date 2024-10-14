3ac30229c06b090cd6b0aca90269c72a5961fb1c
azcryan <andy@canold.com>
2024-10-14 17:00:53 -0600

    See if updating config works

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,25 @@
-    /** @type {import('next').NextConfig} */
-    const nextConfig = {
-        reactStrictMode: true,
-        swcMinify: true,
-        images: {
-            domains: ['lh3.googleusercontent.com', 'localhost'],
-        },
-        webpack(config) {
-            config.experiments = {
-                ...config.experiments,
-                topLevelAwait: true,
-            };
-            return config;
-        },
-    };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        domains: ['lh3.googleusercontent.com', 'localhost'], // Your allowed image domains
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'medflow-mena-health.vercel.app', // Your production domain
+                port: '', // Leave this empty for default ports (80 for HTTP, 443 for HTTPS)
+                pathname: '/**', // This allows all image paths on the domain
+            },
+        ],
+    },
+    webpack(config) {
+        config.experiments = {
+            ...config.experiments,
+            topLevelAwait: true,
+        };
+        return config;
+    },
+};
 
-    export default nextConfig;
+export default nextConfig;

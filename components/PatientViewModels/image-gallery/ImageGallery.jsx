ffc3f84549b5edfdca6e2e85ed4a2836858d65f0
azcryan <andy@canold.com>
2024-10-20 10:29:35 -0600

    Remove duplicate header

--- components/PatientViewModels/image-gallery/ImageGallery.jsx
+++ components/PatientViewModels/image-gallery/ImageGallery.jsx
@@ -206,10 +206,9 @@ const ImageGallery = () => {
 
     return (
         <>
-            <h1 className="text-3xl font-bold mb-8 m-8 text-center">Image Gallery</h1>
             <div className="w-full max-w-4xl mx-auto pb-16">
                 <PatientSubmenu />
-                <div className="border border-gray-300 p-8 bg-white shadow rounded-lg">
+                <div className="border border-gray-300 p-8 my-2 bg-white shadow rounded-lg">
                     <div style={{ minWidth: '75%' }}>
                         {isLoading ? (
                             <div className="flex justify-center items-center h-full">

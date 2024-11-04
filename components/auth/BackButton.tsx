1354a96df46d6c06566cd123e821ec606a5f8130
Kyle Essenmacher <kessenmacher7832@gmail.com>
2024-11-04 17:50:21 -0500

    fixed the back button on the sign up screen

--- components/auth/BackButton.tsx
+++ components/auth/BackButton.tsx
@@ -9,11 +9,10 @@ const BackButton = () => {
     return (
         <Button
             onClick={handleBack}
-            className={`group flex items-center justify-center px-4 py-2 rounded transition-all duration-300
-                bg-gray-200 text-gray-800 shadow-lg shadow-gray-50 border-2 border-gray-200 hover:bg-gray-300 hover:text-gray-900 hover:shadow-gray-200
-            `}
+            className={`group flex items-center justify-center px-4 py-2 rounded`}
+            variant="outline"
         >
-            <UndoDot className="h-5 w-5 text-gray-800 group-hover:text-gray-900 transition-colors duration-300" />
+            <UndoDot className="h-5 w-5" />
         </Button>
     );
 };

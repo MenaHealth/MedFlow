a23da7d263595cc9d57227ea6051d2dbc4dbb92b
Kyle Essenmacher <kessenmacher7832@gmail.com>
2024-11-12 03:05:46 -0500

    Added in ApiWrapper to intercept all api calls for toasts (#65)

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,27 +5,28 @@
 import "@/styles/globals.css";
 import LayoutWrapper from "@/components/LayoutWrapper";
 import { Providers } from "@/components/Providers";
-
-// export const metadata = {
-//     title: "MedFlow",
-//     description: "Connecting patients with volunteers.",
-// };
+import { ToastContext, useToastState, ToastComponent } from '@/components/hooks/useToast';
 
 export default function RootLayout({
                                        children,
                                    }: {
     children: React.ReactNode
 }) {
+    const toastState = useToastState();
+
     return (
         <html lang="en" className="h-full w-full">
         <body className="h-full w-full flex flex-col">
         <Providers>
-            <div className="relative w-full">
-                <div className="gradient absolute inset-0" />
-            </div>
-            <LayoutWrapper>
-                {children}
-            </LayoutWrapper>
+            <ToastContext.Provider value={toastState}>
+                <div className="relative w-full">
+                    <div className="gradient absolute inset-0" />
+                </div>
+                <LayoutWrapper>
+                    {children}
+                </LayoutWrapper>
+                <ToastComponent />
+            </ToastContext.Provider>
         </Providers>
         </body>
         </html>

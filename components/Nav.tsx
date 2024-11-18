0df68f956fb6b4f0c620c7f0240d7e4df6a4b17e
mhatt212 <162051691+mhatt212@users.noreply.github.com>
2024-11-18 17:54:53 -0500

    Update Nav.tsx

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -56,7 +56,7 @@ export default function Nav() {
                     <>
                         <div className="hidden md:flex items-center space-x-4">
                             <NavItem href="/new-patient">New Patient</NavItem>
-                            <NavItem href="/about">Overview</NavItem>
+                            <NavItem href="/overview">Overview</NavItem>
                             <NavItem href="/about">About</NavItem>
                             <NavItem href="/auth">Login</NavItem>
                         </div>
@@ -95,7 +95,7 @@ export default function Nav() {
             {!session?.user && isMobileMenuOpen && (
                 <div className="md:hidden mt-4 space-y-2">
                     <NavItem href="/new-patient">New Patient</NavItem>
-                    <NavItem href="/about">Overview</NavItem>
+                    <NavItem href="/overview">Overview</NavItem>
                     <NavItem href="/about">About</NavItem>
                     <NavItem href="/auth">Login</NavItem>
                 </div>

7593cf86f932ce878321129b72f1947451e4859b
Kyle Essenmacher <kessenmacher7832@gmail.com>
2024-11-19 12:54:23 -0500

    fixed pdf + jpg downloading for rx order, added in copy URL + copy message buttons (#80)
    * fixed the download jpg and pdf buttons, added in the copy link + copy message buttons

--- app/rx-order/[uuid]/page.tsx
+++ app/rx-order/[uuid]/page.tsx
@@ -1,7 +1,7 @@
 // app/rx-order/page.tsx
 'use client';
 
-import QRCodeDisplay from '@/components/Pharmacist/QRCodeDisplay';
+import PatientView from '@/components/rxQrCode/PatientView';
 
 export default function RXOrderPage({ params }: { params: { uuid: string } }) {
     const { uuid } = params; // Extract the `uuid` from dynamic route params
@@ -12,7 +12,7 @@ export default function RXOrderPage({ params }: { params: { uuid: string } }) {
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-            <QRCodeDisplay uuid={uuid} />
+            <PatientView uuid={uuid} />
         </div>
     );
 }
--- app/rx-order/pharmacist/[uuid]/page.tsx
+++ app/rx-order/pharmacist/[uuid]/page.tsx
@@ -1,14 +1,8 @@
 // app/rx-order/pharmacist/[uuid]page.tsx
 'use client';
 
-import PharmacistView from '@/components/Pharmacist/PharmacistView';
+import PharmacistView from '@/components/rxQrCode/PharmacistView';
 
-export default function PharmacistPage({ params }: { params: { uuid: string } }) {
-    const { uuid } = params;
-
-    if (!uuid) {
-        return <p className="text-red-500 text-center mt-8">No prescription UUID provided.</p>;
-    }
-
-    return <PharmacistView uuid={uuid} />;
+export default function PharmacistRxViewPage({ params }: { params: { uuid: string } }) {
+    return <PharmacistView uuid={params.uuid} />;
 }

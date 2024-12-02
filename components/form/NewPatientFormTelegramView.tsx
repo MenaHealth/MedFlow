5925c4b1514be2c0f96adf034fad4bc3a2972805
azcryan <andy@canold.com>
2024-12-02 11:08:15 -0600

    Add patient relation, refactor for data flow

--- components/form/NewPatientFormTelegramView.tsx
+++ components/form/NewPatientFormTelegramView.tsx
@@ -3,6 +3,7 @@
 "use client";
 
 import { FormProvider } from "react-hook-form";
+import { useState } from "react";
 import { TextFormField } from "@/components/ui/TextFormField"; // Your updated TextFormField
 import { SelectFormField } from "./SelectFormField";
 import { PhoneFormField } from "@/components/form/PhoneFormField";
@@ -28,19 +29,29 @@ type NewPatientFormTelegramViewProps = {
     language: "english" | "arabic" | "farsi" | "pashto";
     initialData?: Partial<NewPatientFormTelegramValues>;
     patientId?: string; // Pass patientId to fetch data
+    setFormDataState: (data: any) => void;
+    setShowModal: (show: boolean) => void;
 };
 
 export function NewPatientFormTelegramView({
-                                               onSubmit,
-                                               language,
-                                               initialData,
-                                               patientId,
-                                           }: NewPatientFormTelegramViewProps) {
+        onSubmit,
+        language,
+        initialData,
+        patientId,
+        setFormDataState,
+        setShowModal
+    }: NewPatientFormTelegramViewProps) {
+        
     const { form, submitting, handleSubmit, loading } = useNewPatientFormTelegramViewModel({
         initialData,
         patientId,
         onSubmit,
+        formLanguage: language,
+        setFormDataState,
+        setShowModal
     });
+
+    const [isPatient, setIsPatient] = useState(true); 
 
     console.log("Form Values:", form.watch()); // Debugging logs
 
@@ -48,6 +59,7 @@ export function NewPatientFormTelegramView({
         return <div>Loading patient data...</div>;
     }
 
+    
     const fieldLabels = {
         firstName: { english: "First Name", arabic: "الاسم الأول", farsi: "نام:", pashto: "لومړی نوم:" },
         lastName: { english: "Last Name", arabic: "العائلة اسم", farsi: "نام خانوادگی:", pashto: "تخلص:" },
@@ -73,6 +85,24 @@ export function NewPatientFormTelegramView({
             farsi: { label: "ترجیح جنسیت پزشک", options: ["زن", "مرد", "هیچ"] },
             pashto: { label: "د ډاکټر جنسیت غوره توب", options: ["ښځه", "نارینه", "هیڅ"] },
         },
+        isPatient: {
+            english: 'Please check this box if you are filling out this form on behalf of the patient',
+            arabic: 'يرجى تحديد هذا المربع إذا كنت تقوم بملء هذا النموذج نيابة عن المريض',
+            farsi: 'اگر از طرف بیمار این فرم را پر می کنید، لطفاً این کادر را علامت بزنید',
+            pashto: 'مهرباني وکړئ دا بکس چیک کړئ که تاسو د ناروغ په استازیتوب دا فورمه ډکه کړئ'
+        },
+        patientRelation: {
+            english: 'What is your relation to the patient?',
+            arabic: 'ما هي علاقتك بالمريض؟',
+            farsi: 'نسبت شما با بیمار چیست؟',
+            pashto: 'ستاسو د ناروغ سره څه اړیکه ده؟'
+        },
+        submitting: {
+            english: ["Submit New Patient", "Submitting..."],
+            arabic: ["إرسال مريض جديد", "تقديم"],
+            farsi: ["ارائه بیمار جدید", "ارائه"],
+            pashto: ["نوی ناروغ وسپاري", "سپارل"]
+        }
     };
 
     return (
@@ -144,10 +174,26 @@ export function NewPatientFormTelegramView({
                         radioOptions={fieldLabels.genderPreference[language].options}
                     />
                 </div>
+                {/* Seventh row - Is Patient or Filling on behalf */}
+                <div className="flex mx-2">
+                    <>
+                        <input type="checkbox" onChange={() => {form.setValue("isPatientFields.isPatient", !isPatient); setIsPatient(!isPatient);}}/>
+                        <span className='mx-2'>{fieldLabels.isPatient[language]}</span>
+                    </>
+                </div>
+                    
+                {/* Eighth row - Patient Relation */}
+                {
+                    !isPatient && (
+                        <>
+                            <TextFormField fieldName="isPatientFields.patientRelation" fieldLabel={fieldLabels.patientRelation[language]} error={form.formState.errors.isPatientFields?.patientRelation?.message}/>
+                        </>
+                    )
+                }
                 <div className="flex justify-center">
-                    <Button type="submit" disabled={submitting}>
+                    <Button type="submit" disabled={submitting} style={{ backgroundColor: 'rgb(71, 140, 143)' }}>
                         <SendHorizonal className="mr-2 h-4 w-4"/>
-                        {submitting ? "Submitting..." : "Submit"}
+                        {submitting ? fieldLabels.submitting[language][1] : fieldLabels.submitting[language][0]}
                     </Button>
                 </div>
             </form>
--- components/form/NewPatientFormTelegramViewModel.tsx
+++ components/form/NewPatientFormTelegramViewModel.tsx
@@ -4,6 +4,10 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { useState, useEffect } from "react";
+import { Countries } from "@/data/countries.enum";
+import { Languages } from "@/data/languages.enum";
+import { mapCountryToEnglish } from "@/utils/mapCountryToEnglish";
+import { mapLanguageToEnglish } from "@/utils/mapLanguageToEnglish";
 
 const newPatientFormTelegramSchema = z.object({
     firstName: z.string().min(1, "First name is required"),
@@ -24,6 +28,18 @@ const newPatientFormTelegramSchema = z.object({
     language: z.string().min(1, "Language is required"),
     chiefComplaint: z.string().min(1, "Please enter the main reason you seek medical care"),
     genderPreference: z.string(),
+    isPatientFields: z.object({
+        isPatient: z.boolean(),
+        patientRelation: z.string().optional(),
+    }).superRefine((data, context) => {
+        if (!data.isPatient && (!data.patientRelation || data.patientRelation.trim() === "")) {
+            context.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: "Patient relation is required",
+                path: ["patientRelation"],
+            });
+        }
+    }),
     createdAt: z.date(),
 });
 
@@ -32,10 +48,13 @@ export type NewPatientFormTelegramValues = z.infer<typeof newPatientFormTelegram
 type UseNewPatientFormTelegramViewModelProps = {
     initialData?: Partial<NewPatientFormTelegramValues>;
     patientId?: string; // Add this to fetch patient data
+    formLanguage: "english" | "arabic" | "farsi" | "pashto";
     onSubmit: (formData: NewPatientFormTelegramValues) => Promise<void>;
+    setFormDataState: (data: any) => void,
+    setShowModal: (showModal: boolean) => void
 };
 
-export function useNewPatientFormTelegramViewModel({ initialData, patientId, onSubmit }: UseNewPatientFormTelegramViewModelProps) {
+export function useNewPatientFormTelegramViewModel({ initialData, patientId, formLanguage, setFormDataState, setShowModal }: UseNewPatientFormTelegramViewModelProps) {
     const [submitting, setSubmitting] = useState(false);
     const [loading, setLoading] = useState(true);
 
@@ -54,6 +73,10 @@ export function useNewPatientFormTelegramViewModel({ initialData, patientId, onS
             language: initialData?.language || '',
             chiefComplaint: initialData?.chiefComplaint || '',
             genderPreference: initialData?.genderPreference || '',
+            isPatientFields: {
+                isPatient: true,
+                patientRelation: '',
+            },
             createdAt: new Date(),
         },
     });
@@ -81,6 +104,10 @@ export function useNewPatientFormTelegramViewModel({ initialData, patientId, onS
                         language: data.patient.language || '',
                         chiefComplaint: data.patient.chiefComplaint || '',
                         genderPreference: data.patient.genderPreference || '',
+                        isPatientFields: {
+                            isPatient: data.patient.isPatient || false,
+                            patientRelation: data.patient.patientRelation || '',
+                        },
                         createdAt: new Date(data.patient.createdAt) || new Date(),
                     });
                 }
@@ -96,8 +123,30 @@ export function useNewPatientFormTelegramViewModel({ initialData, patientId, onS
 
     const handleSubmit = form.handleSubmit(async (data) => {
         setSubmitting(true);
+        if (formLanguage !== "english") {
+            const inputLanguage = data.language; // User's selection in their language
+            const inputCountry = data.country;
+            const mappedLanguageKey = mapLanguageToEnglish(inputLanguage, formLanguage);
+            const mappedCountryKey = mapCountryToEnglish(inputCountry, formLanguage);
+    
+            if (mappedLanguageKey) {
+                // Convert the key to the English equivalent
+                data.language = Languages[mappedLanguageKey];
+            }
+
+            if (mappedCountryKey) {
+                // Convert the key to the English equivalent
+                data.country = Countries[mappedCountryKey];
+            }
+        }
+        const newData = JSON.parse(JSON.stringify(data));
+        newData.isPatient = data.isPatientFields.isPatient;
+        newData.patientRelation = data.isPatientFields.patientRelation;
+        delete newData.isPatientFields;
+        setFormDataState(newData);
+        setShowModal(true);
         try {
-            await onSubmit(data);
+            // await onSubmit(newData);
         } finally {
             setSubmitting(false);
         }

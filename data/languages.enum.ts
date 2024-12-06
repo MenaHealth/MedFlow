5972b7294cb653a57bbc352eb56e02e8e846932d
azcryan <andy@canold.com>
2024-12-05 20:22:53 -0600

    undo changes, restrict on intake form

--- data/languages.enum.ts
+++ data/languages.enum.ts
@@ -4,6 +4,10 @@ export enum Languages {
     ARABIC = 'Arabic',
     FARSI = 'Farsi',
     PASHTO = 'Pashto',
+    TURKISH = 'Turkish',
+    URDU = 'Urdu',
+    SPANISH = 'Spanish',
+    FRENCH = 'French',
 }
 
 export const LanguagesList = Object.values(Languages);
@@ -13,6 +17,10 @@ export enum LanguagesArabic {
     FARSI = 'الفارسية',
     PASHTO = 'البشتو',
     ENGLISH = 'إنجليزي',
+    TURKISH = 'تركي',
+    URDU = 'الأردية',
+    SPANISH = 'الأسبانية',
+    FRENCH = 'فرنسي',
 }
 
 export const LanguagesListArabic = Object.values(LanguagesArabic);
@@ -22,6 +30,10 @@ export enum LanguagesFarsi {
     ARABIC = 'عربی',
     PASHTO = 'پشتو',
     ENGLISH = 'انگلیسی',
+    TURKISH = 'ترکی',
+    URDU = 'اردو',
+    SPANISH = 'اسپانیایی',
+    FRENCH = 'فرانسوی',
 }
 
 export const LanguagesListFarsi = Object.values(LanguagesFarsi)
@@ -31,6 +43,10 @@ export enum LanguagesPashto {
     ARABIC = 'عربي',
     FARSI = 'فارسي',
     ENGLISH = 'انګلیسي',
+    TURKISH = 'ترکي',
+    URDU = 'اردو',
+    SPANISH = 'هسپانیه ایی',
+    FRENCH = 'فرانسوي',
 }
 
 export const LanguagesListPashto = Object.values(LanguagesPashto)

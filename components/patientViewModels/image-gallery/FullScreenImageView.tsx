9e8e14ab757eed7623ea251e2fc448be11edd401
Kyle Essenmacher <kessenmacher7832@gmail.com>
2024-12-13 02:03:30 -0500

    updated UX in full screen image view

--- components/patientViewModels/image-gallery/FullScreenImageView.tsx
+++ components/patientViewModels/image-gallery/FullScreenImageView.tsx
@@ -1,8 +1,9 @@
 // components/patientViewModels/image-gallery/FullScreenImageView.tsx
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 import { X, ChevronLeft, ChevronRight } from 'lucide-react';
 import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
+import { Button } from '@/components/ui/button';
 
 interface FullScreenImageViewProps {
     photo: {
@@ -14,44 +15,100 @@ interface FullScreenImageViewProps {
 }
 
 const FullScreenImageView: React.FC<FullScreenImageViewProps> = ({ photo, onClose, onPrev, onNext }) => {
+    const [touchStart, setTouchStart] = useState<number | null>(null);
+    const [touchEnd, setTouchEnd] = useState<number | null>(null);
+    const minSwipeDistance = 50;
+    const containerRef = useRef<HTMLDivElement>(null);
+
     useEffect(() => {
-        const handleEscapeKey = (event: KeyboardEvent) => {
-            if (event.key === 'Escape') {
-                onClose();
+        const handleKeyDown = (event: KeyboardEvent) => {
+            switch (event.key) {
+                case 'Escape':
+                    onClose();
+                    break;
+                case 'ArrowLeft':
+                    onPrev();
+                    break;
+                case 'ArrowRight':
+                    onNext();
+                    break;
             }
         };
 
-        document.addEventListener('keydown', handleEscapeKey);
+        document.addEventListener('keydown', handleKeyDown);
 
         return () => {
-            document.removeEventListener('keydown', handleEscapeKey);
+            document.removeEventListener('keydown', handleKeyDown);
         };
-    }, [onClose]);
+    }, [onClose, onPrev, onNext]);
+
+    const onTouchStart = (e: React.TouchEvent) => {
+        setTouchEnd(null);
+        setTouchStart(e.targetTouches[0].clientX);
+    };
+
+    const onTouchMove = (e: React.TouchEvent) => setTouchEnd(e.targetTouches[0].clientX);
+
+    const onTouchEnd = () => {
+        if (!touchStart || !touchEnd) return;
+        const distance = touchStart - touchEnd;
+        const isLeftSwipe = distance > minSwipeDistance;
+        const isRightSwipe = distance < -minSwipeDistance;
+        if (isLeftSwipe) {
+            onNext();
+        } else if (isRightSwipe) {
+            onPrev();
+        }
+    };
+
+    const handleContainerClick = (e: React.MouseEvent) => {
+        if (e.target === containerRef.current) {
+            console.log("Clicked outside the photo. Closing full screen view.");
+            onClose();
+        }
+    };
+
     return (
-        <div className="fixed inset-0 bg-black bg-opacity-90 z-50 flex items-center justify-center">
-            <button
-                className="absolute top-4 right-4 text-white"
+        <div
+            ref={containerRef}
+            className="fixed inset-0 bg-black bg-opacity-90 z-50 flex items-center justify-center"
+            onTouchStart={onTouchStart}
+            onTouchMove={onTouchMove}
+            onTouchEnd={onTouchEnd}
+            onClick={handleContainerClick}
+        >
+            <Button
+                className="absolute top-4 right-4 z-10"
                 onClick={onClose}
                 aria-label="Close full screen view"
+                variant={'orangeOutline'}
             >
                 <X size={24} />
-            </button>
-            <button
-                className="absolute left-4 top-1/2 transform -translate-y-1/2 text-white"
+            </Button>
+            <Button
+                className="absolute left-4 top-1/2 transform -translate-y-1/2 z-10"
                 onClick={onPrev}
+                aria-label="Previous image"
+                variant={'orangeOutline'}
             >
                 <ChevronLeft size={36} />
-            </button>
-            <button
-                className="absolute right-4 top-1/2 transform -translate-y-1/2 text-white"
+            </Button>
+            <Button
+                className="absolute right-4 top-1/2 transform -translate-y-1/2 z-10"
                 onClick={onNext}
+                aria-label="Next image"
+                variant={'orangeOutline'}
             >
                 <ChevronRight size={36} />
-            </button>
-            <div className="w-full h-full flex items-center justify-center">
+            </Button>
+
+            <div className="relative">
                 <TransformWrapper>
                     <TransformComponent>
-                        <div className="relative w-[80vw] h-[80vh]">
+                        <div
+                            className="relative w-[80vw] h-[80vh]"
+                            onClick={(e) => e.stopPropagation()} // Stops click inside image from closing view
+                        >
                             <Image
                                 src={photo.url}
                                 alt="Full screen image"
@@ -67,4 +124,3 @@ const FullScreenImageView: React.FC<FullScreenImageViewProps> = ({ photo, onClos
 };
 
 export default FullScreenImageView;
-

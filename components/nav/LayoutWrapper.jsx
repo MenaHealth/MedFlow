ad6b01af157c38da2644813bb6ce1d809989b9be
Kyle Essenmacher <kessenmacher7832@gmail.com>
2024-12-16 15:35:38 -0500

    removed excess files, organized component directory a bit, and chnaged imports where needed

---
+++ components/nav/LayoutWrapper.jsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { usePathname } from 'next/navigation';
+import Footer from "@/components/nav/Footer";
+import Nav from "@/components//nav/Nav";
+
+const LayoutWrapper = ({ children }) => {
+    const pathname = usePathname();
+
+    const isFullWidth = pathname === '/patient-info/dashboard';
+
+    return (
+        <div className="flex-grow flex flex-col">
+        <main className={`w-full ${isFullWidth ? 'px-2' : 'max-w-6xl mx-auto'} flex-grow`}>
+            <Nav />
+            <div className="flex-grow">{children}</div>
+        </main>
+        <Footer className={`w-full ${isFullWidth ? '' : 'max-w-6xl mx-auto'}`} />
+        </div>
+    );
+};
+
+export default LayoutWrapper;

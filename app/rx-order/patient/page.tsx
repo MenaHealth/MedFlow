3a5ae3da616b6aae7f081f241e69f515659c87e3
Kyle Essenmacher <kessenmacher7832@gmail.com>
2024-12-16 23:37:37 -0500

    wip

---
+++ app/rx-order/patient/page.tsx
@@ -0,0 +1,18 @@
+// app/rx-order/patient/[id]/page.tsx
+'use client';
+
+import PatientView from '@/components/rxQrCode/PatientRxView';
+
+export default function RXOrderPage({ params }: { params: { uuid: string } }) {
+    const { uuid } = params; // Extract the `uuid` from dynamic route params
+
+    if (!uuid) {
+        return <p className="text-center text-red-500">Invalid or missing UUID</p>;
+    }
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            <PatientView uuid={uuid} />
+        </div>
+    );
+}
